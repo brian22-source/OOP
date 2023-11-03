5c548138860c18ba8b8dcf95b6994578f3680f75
19carty <143806608+19carty@users.noreply.github.com>
2023-11-03 21:33:40 +0300

    first cmmit

---
+++ public method.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+
+# Create an instance of the Calculator class
+calc = Calculator()
+
+# Call the public method 'add' to perform addition
+result = calc.add(5, 3)
+
+# Print the result
+print("The result of addition is:", result)
